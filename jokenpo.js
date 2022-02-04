3e2b3444dd70cd8008f7be91343557d01103a752
SuiCarrot <brunoh.luka@gmail.com>
2022-02-03 21:28:40 -0300

    Versão final

--- jokenpo.js
+++ jokenpo.js
@@ -1,13 +1,18 @@
 const prompt = require("prompt-sync")();
 
+//Array para seleção
+
 const selecao = ["PEDRA", "PAPEL", "TESOURA"];
 
-
+// Início do jogo, e retorno caso o jogador decida jogar de novo
 
 while (true) {
-  let vitorias = 0;
+  //Definição de variáveis de controle do jogo
+  let vitorias = 0; 
   let derrotas = 0;
   let empates = 0;
+
+  //Seleção e validação da quantidade de rodadas escolhida pelo jogador
   console.log(`Quantas rodadas você quer jogar?`);
   let rodadas = +prompt(``).trim().toUpperCase();
   while(true){
@@ -17,8 +22,11 @@ while (true) {
       rodadas = prompt(``)
     }
   }
+
+  //Laço de repetição das rodadas, baseado na escolha do jogador
   for (i = 0; i < rodadas; i++) {
     console.log(`-----------------------------------------------------------------`)
+    // Escolha e validação da escolha do jogador, baseado no array de seleção
     console.log(`Escolha entre Pedra, Papel e Tesoura:`);
     let escolhaPlayer = prompt(``).trim().toUpperCase();
 
@@ -29,17 +37,18 @@ while (true) {
         escolhaPlayer = prompt(``).trim().toUpperCase();
       }
     }
-
+    // Escolha aleatória da maquina, e transformação da escolha do jogador de string para numero de indice no array
     let escolhaPc = Math.floor(Math.random() * 3);
     let selecaoComp = selecao[escolhaPc];
     let selecaoPlayer = selecao[selecao.indexOf(escolhaPlayer)];
     escolhaPlayer = selecao.indexOf(escolhaPlayer);
 
+    // Validações e comparações das duas escolhas para definir o vencedor da rodada, utilizando as posições relativas no array de seleção.
+    // Se forem iguais, empate. O numero de indice maior ganha. Com exceção da primeira posição, que ganha da ultima
     if (escolhaPc === escolhaPlayer) {
       console.log(
         `\nVocê escolheu: ${selecaoPlayer}\nEu escolhi: ${selecaoComp}\nNesta rodada vocês empataram.`
       );
-      console.log(``);
       empates++;
     } else if (escolhaPlayer == 0 && escolhaPc == 2) {
       console.log(
@@ -63,11 +72,11 @@ while (true) {
       derrotas++;
     }
   }
-
+  // Exibição do placar, baseado nas rodadas.
   console.log(
-    `Você ganhou ${vitorias} vezes.\n Eu ganhei ${derrotas} vezes.\nEmpatamos ${empates} vezes.`
+    `Você ganhou ${vitorias} vezes.\nEu ganhei ${derrotas} vezes.\nEmpatamos ${empates} vezes.`
   );
-
+  // Exibição do campeão se houver um.
   if (vitorias > derrotas) {
     console.log(`\nParabéns, você venceu com ${vitorias} vitórias.`);
   } else if (vitorias < derrotas) {
@@ -81,10 +90,10 @@ while (true) {
   }
 
   console.log(``);
-
+  // Escolha do jogador de jogar novamente ou não. Se decidir jogar novamente o programa reinicia.
   console.log(`\nDeseja jogar novamente? `);
   let jogarNovamente = prompt(``).trim().toUpperCase();
-  if (jogarNovamente != "SIM") break;
+  if (jogarNovamente != "SIM" && jogarNovamente!= "S") break;
   console.clear();
 }
 console.clear();
